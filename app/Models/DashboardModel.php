9f9b9134a59990147179d34e37381b4c5e9b9549
haafidz-jp <haafidz123@gmail.com>
2021-10-13 22:48:52 +0700

    ganti sql clause di comment

--- app/Models/DashboardModel.php
+++ app/Models/DashboardModel.php
@@ -29,13 +29,13 @@ class DashboardModel extends Model
         return $this->builder->getWhere(['kuantitas <' => 10,'satuan' => 'PCS']) ->getNumRows();
     }
 
-    // fungsi SELECT * FROM produk WHERE quantity > 50;
+    // fungsi SELECT * FROM produk WHERE quantity > 50 AND satuan = 'PAK';
     public function produk_lebih_card_pak()
     {
         return $this->builder->getWhere(['kuantitas >' => 50,'satuan' => 'PAK']) ->getNumRows();
     }
     
-    // fungsi SELECT * FROM produk WHERE quantity > 50;
+    // fungsi SELECT * FROM produk WHERE quantity > 50 AND satuan = 'PCS';
     public function produk_lebih_card_pcs()
     {
         return $this->builder->getWhere(['kuantitas >' => 50,'satuan' => 'PCS']) ->getNumRows();
@@ -43,26 +43,26 @@ class DashboardModel extends Model
 
 
     // TABEL KURANG
-    // fungsi SELECT * FROM produk WHERE quantity < 10;
+    // fungsi SELECT * FROM produk WHERE quantity < 10 AND satuan ='PAK';
     public function produk_kurang_pak()
     {
         return $this->builder->getWhere(['kuantitas <' => 10,'satuan' => 'PAK'])->getResultObject();
     }
 
-    // fungsi SELECT * FROM produk WHERE quantity < 10;
+    // fungsi SELECT * FROM produk WHERE quantity < 10 AND satuan ='PCS';
     public function produk_kurang_pcs()
     {
         return $this->builder->getWhere(['kuantitas <' => 10,'satuan' => 'PCS'])->getResultObject();
     }
 
     // TABEL LEBIH
-    // fungsi SELECT * FROM produk WHERE quantity > 50;
+    // fungsi SELECT * FROM produk WHERE quantity > 20 AND satuan ='PAK';
     public function produk_lebih_pak()
     {
         return $this->builder->getWhere(['kuantitas >' => 20,'satuan' => 'PAK'])->getResultObject();
     }
     
-    // fungsi SELECT * FROM produk WHERE quantity > 50;
+    // fungsi SELECT * FROM produk WHERE quantity > 20 AND satuan ='PCS';
     public function produk_lebih_pcs()
     {
         return $this->builder->getWhere(['kuantitas >' => 20,'satuan' => 'PCS'])->getResultObject();
